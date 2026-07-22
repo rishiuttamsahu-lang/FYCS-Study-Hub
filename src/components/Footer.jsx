eb641cf89756647f59be9e5c59e237baf1469a6e
Rishi Sahu <rishiuttamsahu@gmail.com>
2026-07-22 11:27:51 +0530

    firebase.js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <p className="text-center text-xs text-white/50 py-4 font-medium tracking-wide">
+    <p className="text-center text-[11px] sm:text-xs text-white/50 py-4 font-medium tracking-wide">
       Developed by Sahu Rishikesh &copy; 2026
     </p>
   );
